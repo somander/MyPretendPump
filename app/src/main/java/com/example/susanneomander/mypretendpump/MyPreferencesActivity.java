e9656a67e0c6696a5dcd488d6c1319d675136f99
Susanne <susanne.omander@gmail.com>
2017-04-02 13:45:23 -0700

    Added Back arrow in Settings

--- app/src/main/java/com/example/susanneomander/mypretendpump/MyPreferencesActivity.java
+++ app/src/main/java/com/example/susanneomander/mypretendpump/MyPreferencesActivity.java
@@ -3,12 +3,13 @@ package com.example.susanneomander.mypretendpump;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 
-public class MyPreferencesActivity extends PreferenceActivity {
+/*public class MyPreferencesActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+       super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
 
@@ -22,4 +23,35 @@ public class MyPreferencesActivity extends PreferenceActivity {
         }
     }
 
+}*/
+
+public class MyPreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+
+        try {
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }catch (Exception e) {
+            //setDisplayShowHomeEnabled may throw a NullPointerException
+        }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
 }
